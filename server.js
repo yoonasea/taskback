c668adda35fcc7e2480031f72ca4ee87884290ee
Yoonasea <phuazaiwei@gmail.com>
2024-07-28 14:37:24 +0800

    removed unused file

--- server.js
+++ server.js
@@ -1,17 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const tasks = require('./routes/tasks');
-const fs = require('fs');
-const path = require('path');
 
 const app = express();
-
-const filePath = path.join(__dirname, 'tasks.json');
-
-// Initialize tasks.json file if it doesn't exist
-if (!fs.existsSync(filePath)) {
-  fs.writeFileSync(filePath, JSON.stringify([]));
-}
 
 app.use(cors());
 app.use(express.json());
